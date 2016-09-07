1d6b8848ccb6e511319acc2d74cc21a59d8e38b5
Nuno Cunha <me@nunocunha.pt>
2016-09-07 17:25:54 +0100

    Make it work on Edge Communication between content and background script isn't working at the moment.

--- background-scripts/qr-coder.js
+++ background-scripts/qr-coder.js
@@ -1,73 +1,77 @@
-chrome.extension.isAllowedFileSchemeAccess(function (isAllowedAccess) {
+(!(typeof browser.extension.isAllowedFileSchemeAccess === "undefined")) && browser.extension.isAllowedFileSchemeAccess(function (isAllowedAccess) {
   if (isAllowedAccess) return;
 
   alert('Please allow access to file URLs on the following screen.');
 
-  chrome.tabs.create({
-    url: 'chrome://extensions/?id=' + chrome.runtime.id
+  browser.tabs.create({
+    url: 'chrome://extensions/?id=' + browser.runtime.id
   });
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-selection",
   title: "Generate a QR Code for this selection",
   contexts: ['selection']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-link",
   title: "Generate a QR Code for this link",
   contexts: ['link']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-page",
   title: "Generate a QR Code for this page",
   contexts: ['page']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-image",
   title: "Generate a QR Code for this image",
   contexts: ['image']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-audio",
   title: "Generate a QR Code for this sound",
   contexts: ['audio']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-video",
   title: "Generate a QR Code for this video",
   contexts: ['video']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: "qr-coder-frame",
   title: "Generate a QR Code for this frame",
   contexts: ['frame']
 });
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+  var code;
+
   switch (info.menuItemId) {
     case "qr-coder-selection":
-      chrome.tabs.executeScript(tab.id, { code: "QRCoder.showOverlay('', true);" });
+      code = "QRCoder.showOverlay('', true);";
       break;
     case "qr-coder-page":
-      chrome.tabs.executeScript(tab.id, { code: "QRCoder.showOverlay('" + window.btoa(info.pageUrl) + "', false);" });
+      code = "QRCoder.showOverlay('" + window.btoa(info.pageUrl) + "', false);";
       break;
     case "qr-coder-link":
-      chrome.tabs.executeScript(tab.id, { code: "QRCoder.showOverlay('" + window.btoa(info.linkUrl) + "', false);" });
+      code = "QRCoder.showOverlay('" + window.btoa(info.linkUrl) + "', false);";
       break;
     case "qr-coder-image":
     case "qr-coder-audio":
     case "qr-coder-video":
-      chrome.tabs.executeScript(tab.id, { code: "QRCoder.showOverlay('" + window.btoa(info.srcUrl) + "', false);" });
+      code = "QRCoder.showOverlay('" + window.btoa(info.srcUrl) + "', false);";
       break;
     case "qr-coder-frame":
-      chrome.tabs.executeScript(tab.id, { code: "QRCoder.showOverlay('" + window.btoa(info.frameUrl) + "', false);" });
+      code = "QRCoder.showOverlay('" + window.btoa(info.frameUrl) + "', false);";
       break;
   }
+
+  browser.tabs.executeScript(tab.id, { code: code });
 });
